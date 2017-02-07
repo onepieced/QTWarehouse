6669a593018f8c62b209ab8174999537570971ba
elong <tao.qi@chemayi.com>
2017-02-07 14:20:45 +0800

    no message

---
+++ QTGit0207/QTGit0207/mainViewController.h
@@ -0,0 +1,13 @@
+//
+//  mainViewController.h
+//  QTGit0207
+//
+//  Created by MBP2010 on 2017/2/7.
+//  Copyright © 2017年 MBP2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mainViewController : UIViewController
+
+@end
---
+++ QTGit0207/QTGit0207/mainViewController.m
@@ -0,0 +1,37 @@
+//
+//  mainViewController.m
+//  QTGit0207
+//
+//  Created by MBP2010 on 2017/2/7.
+//  Copyright © 2017年 MBP2010. All rights reserved.
+//
+
+#import "mainViewController.h"
+
+@interface mainViewController ()
+
+@end
+
+@implementation mainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
