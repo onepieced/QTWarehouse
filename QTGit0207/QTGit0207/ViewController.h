460905e0d6a5c2d40a1df155945f1bf2f0134bf9
elong <tao.qi@chemayi.com>
2017-02-07 11:49:53 +0800

    添加工程

---
+++ QTGit0207/QTGit0207/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QTGit0207
+//
+//  Created by MBP2010 on 2017/2/7.
+//  Copyright © 2017年 MBP2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QTGit0207/QTGit0207/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  QTGit0207
+//
+//  Created by MBP2010 on 2017/2/7.
+//  Copyright © 2017年 MBP2010. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
